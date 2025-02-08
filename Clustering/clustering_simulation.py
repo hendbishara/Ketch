cc0286739cc98488e535be727856e644f18d7f1e
Abdallahs21 <100430281+Abdallahs21@users.noreply.github.com>
2025-02-08 21:17:05 +0200

    Added send note feature, with few changes

--- Clustering/clustering_simulation.py
+++ Clustering/clustering_simulation.py
@@ -1,8 +1,8 @@
 import random
 from geopy.distance import geodesic
 import db_methods
-#from cluster_manager import ClusterManager
 from cluster_manager2 import ClusterManager
+import Send_note
 
 class Simulation:
     def __init__(self, cluster_manager):
@@ -14,6 +14,11 @@ cluster_manager2 = ClusterManager(1,20)  # Initialize with desired radius
 cluster_manager2.build_clusters()  # Build initial clusters from existing data
 cluster_manager2.create_map()
 
+# Example Usage
+# Assume clusters are built already
+#for cluster in cluster_manager2.get_clusters():
+    
+#    Send_note.notify_users_about_cluster_items(cluster)
 '''
 simulation = Simulation(cluster_manager)
 simulation.run_simulation(50)  # Simulate adding 50 random orders
