2d1e3cafa26afa03601444a25ccefff799da093b
Abdallahs21 <100430281+Abdallahs21@users.noreply.github.com>
2025-02-10 14:34:46 +0200

    filtered files, kept the relevant code

--- Clustering/clustering_simulation.py
+++ Clustering/clustering_simulation.py
@@ -1,19 +1,31 @@
 import random
 from geopy.distance import geodesic
 import db_methods
-from cluster_manager2 import ClusterManager
+from Clustering.cluster_manager import ClusterManager
 import Send_note
+from routing import RoutePlanner
+import openrouteservice
+
+API_KEY = "5b3ce3597851110001cf624801095298be2b4fd9aad1382911873455"
 
 class Simulation:
     def __init__(self, cluster_manager):
         self.cluster_manager = cluster_manager
 
+    def run_clustering(self): 
+        self.cluster_manager.build_clusters()  # Build initial clusters from existing data
+        self.cluster_manager.create_map()
+        return self.cluster_manager.get_clusters()
+    def run_routing(self):
+        planner = RoutePlanner(API_KEY, self.cluster_manager)
+        planner.draw_routes_between_neighbors()
+
 
 # Simulation usage:
-cluster_manager2 = ClusterManager(1,20)  # Initialize with desired radius
-cluster_manager2.build_clusters()  # Build initial clusters from existing data
-cluster_manager2.create_map()
-
+simulation = Simulation(ClusterManager(1, 20))
+clusters = simulation.run_clustering()
+planner = RoutePlanner(API_KEY, simulation.cluster_manager)
+planner.build_graph()
 # Example Usage
 # Assume clusters are built already
 #for cluster in cluster_manager2.get_clusters():
